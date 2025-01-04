2522cb6126f739301fa1ab600b04839fd8a13cdf
Dinesh_DC <dineshshinde81563@gmail.com>
2025-01-04 22:50:44 +0530

    Practice the routing and made some routes and render componenets

--- RoutersPractice/src/App.jsx
+++ RoutersPractice/src/App.jsx
@@ -14,7 +14,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
+          <Route path="/services" element={<Services />} />  {/* When the URL is /services */}
+          <Route path="/services/:category" element={<Services />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
--- RoutersPractice/src/components/Services.jsx
+++ RoutersPractice/src/components/Services.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+// import api from "../utlitity/AxiosConfig";
+
 const Services = () => {
+
+  const getproducts = async ()=>{
+    try {
+      const data = await api.get("/products")
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#3c3c3c] text-white p-4">
-      <h1 className="text-4xl font-bold mb-4">Our Services</h1>
+      <h1 className="text-4xl font-bold mb-4">Our Servi ces</h1>
       <p className="text-lg mb-4">
         We offer a wide range of services to meet your needs, including web
         development, mobile app development, and more.
@@ -14,7 +25,25 @@ const Services = () => {
         <li className="mb-2">UI/UX Design</li>
         <li className="mb-2">SEO Optimization</li>
       </ul>
+
+    <ul>
+      {/* {
+        getproducts().map((product, index) => {
+          return (
+            <li key={index} className="mb-2">
+              {product.name}
+            </li>
+          );
+        })
+      } */}
+    </ul>
+
+
+     
     </div>
+
+
+
   );
 };
 
